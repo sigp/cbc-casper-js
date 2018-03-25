a91927c140051f07bebb0de7b781725760630baf
Paul Hauner <paul@paulhauner.com>
2018-03-25 21:45:23 +1100

    Update binary sim to first working version

--- sims/binary.js
+++ sims/binary.js
@@ -14,12 +14,21 @@ const popRandomElement = function(a) {
 	return i;
 }
 
-const rand = function(rounds, messagesPerRound) {
-	const validators = [
-		new Validator(name="Andy", weight=100, startingPoint=0),
-		new Validator(name="Brian", weight=100, startingPoint=0),
-		new Validator(name="Chris", weight=100, startingPoint=1),
+const rand = function(requiredSafetyRatio, messagesPerRound) {
+	const validatorInfo = [
+		{name: "Andy", weight: 100, startingPoint: 0},
+		{name: "Brian", weight: 100, startingPoint: 0},
+		{name: "Chris", weight: 100, startingPoint: 1},
 	]
+	const validators = validatorInfo.map(v => {
+		return new Validator(
+			name=v.name,
+			weight=v.weight,
+			startingPoint=v.startingPoint,
+		);
+	});
+	validators.forEach(v => v.learnValidators(validatorInfo));
+
 	let n = new Network(validators);
 
 	const doRound = function(messages) {
@@ -37,12 +46,24 @@ const rand = function(rounds, messagesPerRound) {
 			});
 		});
 	}
-
-	for(var i = 0; i < rounds; i++) {
+	
+	let consensusAchieved = false;
+	while(consensusAchieved === false) {
 		doRound(messagesPerRound);
+		consensusAchieved = true;
+		validators.forEach(v => {
+			console.log(v.getEstimate().safety)
+			if(v.getEstimate().safety <= requiredSafetyRatio) {
+				consensusAchieved = false;
+			}
+		});
 	}
 
-	console.log(n.getLog())
+	const output = {
+		log: n.getLog()
+	}
+
+	return output;
 };
 
-rand(5, 1);
+console.log(rand(0.5, 1));
