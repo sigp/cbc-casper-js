e95eb197b5263b9b5dc9d4d9464f7ba1ec7989fd
Paul Hauner <paul@paulhauner.com>
2018-04-07 23:24:43 +1000

    Add test to ensure consecutive message consistancy

--- test/validators/messageGeneration.js
+++ test/validators/messageGeneration.js
@@ -1,6 +1,7 @@
 var hashObj = require('object-hash');
 var assert = require("assert");
-var BinaryValidator = require("../../validators/binary")
+var BinaryValidator = require("../../validators/binary");
+var MsgDB = require("../../db");
 
 
 describe('Validator message generation', function() {
@@ -61,6 +62,62 @@ describe('Validator message generation', function() {
 			hashObj(generated),
 			hashObj(expected),
 			'the generated message should be as expected'
+		)
+	});
+	
+	it('should return the same message if nothing has changed', function() {
+		const msg = {
+			sender: 'Brian',
+			estimate: 0,
+			justification: [
+				{
+					sender: 'Brian',
+					estimate: 1,
+					justification: [],
+				},
+				{
+					sender: 'Sally',
+					estimate: 0,
+					justification: [],
+				}
+			],
+		};
+		let v = new BinaryValidator('Test', 100, 0);
+		v.parseMsg(msg);
+		const first = hashObj(v.generateMsg());
+		const second = hashObj(v.generateMsg());
+		assert.notEqual(
+			first,
+			second,
+			'two successively generated messages should be the same'
+		)
+	});
+	
+	it('should store a generated message as its latest msg', function() {
+		const msg = {
+			sender: 'Brian',
+			estimate: 0,
+			justification: [
+				{
+					sender: 'Brian',
+					estimate: 1,
+					justification: [],
+				},
+				{
+					sender: 'Sally',
+					estimate: 0,
+					justification: [],
+				}
+			],
+		};
+		let v = new BinaryValidator('Test', 100, 0);
+		v.parseMsg(msg);
+		const db = new MsgDB();
+		const generated = db.store(v.generateMsg());
+		assert.equal(
+			v.getLatestMsgHash(v.name),
+			generated,
+			'the validator should know its last message as its latest'
 		)
 	});
 });
