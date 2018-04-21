c87fbfc1a5b77f63d12b8b47769031ebe9e0b018
Paul Hauner <paul@paulhauner.com>
2018-04-21 16:02:00 +1000

    Fix msg processing tests to work with new db

--- test/validators/messageProcessing.js
+++ test/validators/messageProcessing.js
@@ -1,32 +1,41 @@
 var hashObj = require('object-hash');
 var assert = require("assert");
 var BinaryValidator = require("../../validators/binary")
+var Network = require("../../network");
+var MsgDB = require("../../db");
 
 
 describe('Validator message processing', function() {
 
 	it('should place the first message from a sender in the table', function() {
-		const msg = {
+		let db = new MsgDB();
+		let v = new BinaryValidator('Test', 0, 0, db);
+
+		const msg = db.store({
 			sender: 'Brian',
 			estimate: 1,
 			justification: [],
-		}
-		let v = new BinaryValidator('Test', 0, 0);
+		});
+
 		v.parseMsg(msg);
+
 		assert.equal(
 			v.getLatestMsgHash('Brian'),
-			v.storeMsg(msg, {}), 
+			msg, 
 			'the sent message should be the latest messsage.'
 		);
 	});
 
 	it('should update last message when parsing a new message from a known sender', function() {
-		const msg1 = {
+		let db = new MsgDB();
+		let v = new BinaryValidator('Test', 0, 0, db);
+
+		const msg1 = db.store({
 			sender: 'Brian',
 			estimate: 0,
 			justification: [],
-		};
-		const msg2 = {
+		});
+		const msg2 = db.store({
 			sender: 'Brian',
 			estimate: 0,
 			justification: [
@@ -41,31 +50,35 @@ describe('Validator message processing', function() {
 					justification: [],
 				}
 			],
-		};
-		let v = new BinaryValidator('Test', 0, 0);
-		// parse the first message
+		});
+
 		v.parseMsg(msg1);
+
 		assert.equal(
 			v.getLatestMsgHash('Brian'),
-			v.storeMsg(msg1, {}), 
+			msg1, 
 			'the sent message should be the latest messsage.'
 		);
-		// parse the second message
+
 		v.parseMsg(msg2);
+
 		assert.equal(
 			v.getLatestMsgHash('Brian'),
-			v.storeMsg(msg2, {}), 
+			msg2, 
 			'the latest message should have been updated.'
 		);
 	});
 	
 	it('should not update the last message when parsing a known dependency message', function() {
-		const msg1 = {
+		let db = new MsgDB();
+		let v = new BinaryValidator('Test', 0, 0, db);
+
+		const msg1 = db.store({
 			sender: 'Brian',
 			estimate: 0,
 			justification: [],
-		};
-		const msg2 = {
+		});
+		const msg2 = db.store({
 			sender: 'Brian',
 			estimate: 0,
 			justification: [
@@ -80,20 +93,21 @@ describe('Validator message processing', function() {
 					justification: [],
 				}
 			],
-		};
-		let v = new BinaryValidator('Test', 0, 0);
-		// parse the most recent message
+		});
+
 		v.parseMsg(msg2);
+
 		assert.equal(
 			v.getLatestMsgHash('Brian'),
-			v.storeMsg(msg2, {}), 
+			msg2, 
 			'the sent message should be the latest messsage.'
 		);
-		// parse a dependency of the previous message
+
 		v.parseMsg(msg1);
+
 		assert.equal(
 			v.getLatestMsgHash('Brian'),
-			v.storeMsg(msg2, {}), 
+			msg2, 
 			'the latest message should not have been updated.'
 		);
 	});
